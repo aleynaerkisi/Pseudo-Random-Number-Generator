9a2abdd26bbad5d7b3d8efbd97cbdeb3cf3627e2
Aleyna Erkisi <alyeynaerkisi@gmail.com>
2022-03-30 11:17:29 +0300

    Comment Fix
    commentler fixlendi daha kolay okunur olması icin

--- RandomNumberGenerator.py
+++ RandomNumberGenerator.py
@@ -11,11 +11,11 @@ import pyautogui
 
 
 
-# In[49]:
 
 
-#son kod
-# mouse ın tıklandığı an ve tıklandığı pozisyonun x değerini çarpıp elde ettiği sayıyı file basıyor
+
+
+# mouse ın tıklandığı an ve tıklandığı pozisyonun x değerini çarpıp elde ettiği sayıyı file a basıyor
 
 a=1
 f = open("Seeds.txt", "a")
@@ -23,14 +23,14 @@ while a<7:
     check= win32api.GetKeyState(0x01) #mouse sol click virtual key kodu
     if check < 0:
         print("left button is clicked")
-        mtime= round(time.time()) #tam bir sayıya yuvarlıyor ki noktalı olmasın sayı
+        mtime= round(time.time()) #tam bir sayıya yuvarlama
         x, y = pyautogui.position() #mouse ın pozisyonu     
         final=mtime*int(str(x).rjust(4)) #pozisyonu ve saati çarpıyor
-        f.write(str(final)) #çarpptığı sayıyı dosyaya basıyor
+        f.write(str(final)) #çarptığı sayıyı dosyaya basıyor
         f.write("\n") #her bastığı sayı sonrası satır boşluğu bırakıyor bu daha sonra cpp kodunda kolaylık olması için
         a+=1 #countı arttırıyor
-        time.sleep(20) #basılı tutarken sürekli basmaması için burayı arttır 20 yeterli şimdilik 7 sayı alacağım için
-                      #random olmasını istiyorum
+        time.sleep(20) 
+                      
 f.close()
 
 
